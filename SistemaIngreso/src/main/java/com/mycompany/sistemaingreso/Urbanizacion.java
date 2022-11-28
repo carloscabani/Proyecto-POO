2f7916cb0d8fd5a158b13d536a7d2bb6e05bea3c
carloscabani <cacabani@espol.edu.ec>
2022-11-27 22:49:48 -0500

    Primer commit, subidad de los cuatro primeros literales

---
+++ SistemaIngreso/src/main/java/com/mycompany/sistemaingreso/Urbanizacion.java
@@ -0,0 +1,33 @@
+
+package com.mycompany.sistemaingreso;
+
+
+public class Urbanizacion {
+    
+    
+    public String nombreUrb;
+    public int etapa;
+    public String emailAdmin;
+    public String direccion;
+    public String constructora;
+    public String admin;
+
+
+    public void setModificarInfo(String nombreUrb, int etapa, String emailAdmin,String direccion, String constructora, String admin){
+        
+        this.nombreUrb= nombreUrb;
+        this.etapa=etapa;
+        this.emailAdmin=emailAdmin;
+        this.direccion=direccion;
+        this.constructora=constructora;
+        this.admin=admin;
+        
+    }
+    
+    
+    
+    
+    
+
+
+}
